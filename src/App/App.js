d30d29af035ea584835e25450b06842ab79d4078
Veronika Martinovich <veranika.mart@gmail.com>
2020-08-19 16:21:08 +0300

    feat: add sass and fonts to the project

---
+++ src/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="app">
+      <main className="main-wrapper">
+        
+      </main>
+    </div>
+  );
+}
+
+export default App;
