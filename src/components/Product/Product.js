0b60641a7d98344fa8373502264cef811514644f
Veronika Martinovich <veranika.mart@gmail.com>
2020-08-20 13:04:48 +0300

    feat: add styles for product container

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -4,18 +4,24 @@ import "./Product.scss";
 export const Product = ({ product }) => {
   return (
     <div className="product">
-      <div className="product__container">
-        <span className="product__title">{product.title}</span>
-        <h3 className="product__name">
-          {product.name}
-          <span className="product__description">{product.description}</span>
-        </h3>
-        {product.additionalInfo.map((item, index) => (
-          <span className="product__additional-info" key={index}>{item}</span>
-        ))}
+      <div className={`product__container-wrapper product__container-wrapper_${product.styles}`}>
+        <div className="product__container">
+          <span className="product__title">{product.title}</span>
+          <h3 className="product__name">
+            {product.name}
+            <span className="product__description">{product.description}</span>
+          </h3>
+          {product.additionalInfo.map((item, index) => (
+            <span className="product__additional-info" key={index}>
+              {item}
+            </span>
+          ))}
+        </div>
       </div>
       {product.comments.map((item, index) => (
-        <span className="product__comment" key={index}>{item.text}</span>
+        <span className="product__comment" key={index}>
+          {item.text}
+        </span>
       ))}
     </div>
   );
